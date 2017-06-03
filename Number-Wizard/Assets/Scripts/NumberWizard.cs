80411e01d37d6fd33e51f163e5f2ded76fc390cc
Jon-PC\Jon <jdlee022@gmail.com>
2017-06-02 23:44:15 -0700

    adding Number Wizard

---
+++ Number-Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    //Use this for initialization
+    int max;
+    int min;
+    int guess;
+    int maxGuessesAllowed = 10;
+
+    public Text text;
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        NextGuess();
+    }
+
+    public void GuessHigher() {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower() {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess() {
+        guess = Random.Range(min, max+1);
+        text.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if(maxGuessesAllowed <= 0) {
+            Application.LoadLevel("Win");
+        }
+
+    }
+}
